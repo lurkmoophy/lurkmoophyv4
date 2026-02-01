b6a925c1ecdeaaa11f70f3857666d7482955b060
Luke Murphy <lurkmoophy@gmail.com>
2026-02-01 17:35:03 +0000

    update rss bit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://lurkmoophy.com',
 	integrations: [mdx(), react(), sitemap()],
 });
