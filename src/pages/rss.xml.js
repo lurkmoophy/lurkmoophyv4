34eea147fc6e8c4c60e6952a44d6c9d9ba613902
Luke Murphy <lurkmoophy@gmail.com>
2026-02-01 17:29:44 +0000

    added rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,15 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
+const parser = new MarkdownIt();
+
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('blog')).sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+	);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
@@ -11,6 +17,9 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.id}/`,
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+			}),
 		})),
 	});
 }
